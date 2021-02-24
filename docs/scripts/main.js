d7db7d0716a59b3e2f47a165df1e51ff096e0b34
Volpanic <volpanicstudios@gmail.com>
2021-02-24 21:53:12 +1100

    Replaced html card code, with a javascript script that autogens cards based on created classes.

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,20 +1,104 @@
-var coll = document.getElementsByClassName("Collapsible");
-var i;
 
-for(i = 0; i < coll.length; i++)
-{
-  coll[i].addEventListener("click",function()
-  { // Funtions stuff here
-    this.classList.toggle("Active");
-    var content = this.nextElementSibling;
+class GameCard {
+  constructor(GameName,MainImage,GameSkills,PlayLink,GithubLink,Images){
+    this.GameName = GameName;
+    this.MainImage = MainImage;
+    this.GameSkills = GameSkills;
+    this.PlayLink = PlayLink;
+    this.GithubLink = GithubLink;
+    this.Images = Images;
+  }
 
-    if(content.style.maxHeight)
-    {
-      content.style.maxHeight = null;
+    DrawCard(ParentElement) {
+      
+      let base = document.createElement('div');
+      base.className = "col-sm6 mx-auto";
+      
+      //Start card div
+      let card = document.createElement('div');
+      card.className = "card text-center mx-auto";
+      card.style = "width: 24rem;";
+
+      base.append(card);
+
+      //Display the covere image
+      let coverImage = document.createElement('img');
+      coverImage.src = this.MainImage;
+      coverImage.className = "card-img-top";
+
+      card.appendChild(coverImage);
+
+      //Card Body
+      let cardBody = document.createElement('div');
+      cardBody.className = "card-body";
+      
+      card.appendChild(cardBody);
+
+      //Title
+      let cardTitle = document.createElement('h5');
+      cardTitle.className = "card-title";
+      cardTitle.innerText = this.GameName;
+
+      cardBody.appendChild(cardTitle);
+
+      //Skills
+      let cardSkills = document.createElement('p');
+      cardSkills.className = "card-text";
+      cardSkills.innerText = this.GameSkills;
+
+      cardBody.appendChild(cardSkills);
+      
+      //Card Inner body, buttons
+      let cardInner = document.createElement('div');
+      cardInner.className = "container-fluid";
+
+      cardBody.appendChild(cardInner);
+
+      //PlayButton
+      if(this.PlayLink != null && this.PlayLink != "")
+      {
+        let pButton = document.createElement('a'); 
+        pButton.href = this.PlayLink;
+        pButton.className = "btn btn-dark";
+        pButton.innerText = "Play";
+        cardInner.appendChild(pButton);
+      }
+
+      //GithubButton
+      if(this.GithubLink != null && this.GithubLink != "")
+      {
+        let gButton = document.createElement('a'); 
+        gButton.href = this.GithubLink;
+        gButton.className = "btn btn-dark";
+        gButton.innerText = "Github";
+        cardInner.appendChild(gButton);
+      }
+
+      let mButton = document.createElement('button'); 
+      //mButton.href = GithubLink;
+      mButton.type = "button";
+      mButton.className = "btn btn-dark";
+      mButton.innerText = "More Images";
+      cardInner.appendChild(mButton);
+
+      ParentElement.appendChild(base);
+
+      return;
     }
-    else
-    {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
 }
+
+var cards = [
+  new GameCard("Nova","Images/Nova/NOVA1.png","Unity, C#, 2D, Solo work, Art, Music.","https://volpanic.github.io/Nova/","https://github.com/Volpanic/Nova",[1,1,1]),
+  new GameCard("Project Still","Images/ProjectStill/Bedroom.png","Unity, C#, 3D, Team Work.",null,"https://github.com/AIESydneyStudents/LucidDreams",[1,1,1]),
+  new GameCard("Schism","Images/Schism/Schism4.png","Game Maker Studio 2, GML, 2D, Solo work, Art, Music, Release.","https://volpanic.itch.io/schism","",[1,1,1]),
+  new GameCard("Fish Game","Images/FishGame/FishGame2.png","C++, Raylib, 2D, Engine Design, AI, Pathfinding, Solo work, Art, Music.","https://github.com/Volpanic/AIForGamesAie","",[1,1,1]),
+  new GameCard("Tank Game","Images/TankGame/TankGame1.png","C#, Raylib, 2D, Math, Unit testing, Solo work, Art, Music.","https://github.com/Volpanic/aieMathsLibrary","",[1,1,1])
+];
+
+var container = document.getElementById("game-cards");
+
+for(var i = 0; i < cards.length; i++)
+{
+  cards[i].DrawCard(container);
+}
+
